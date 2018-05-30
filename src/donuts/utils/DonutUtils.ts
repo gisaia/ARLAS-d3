c8fdb732ca6a26178b9db72f1f44fbb55cb45677
MohamedHamou <mohamed.hamou@gisaia.com>
2018-05-30 15:22:03 +0200

    Bug fix related to selecting donut nodes that doesn't have the same nb of parents

--- src/donuts/utils/DonutUtils.ts
+++ src/donuts/utils/DonutUtils.ts
@@ -69,16 +69,20 @@ export class DonutUtils {
     count--;
     while (count >= 0 && nodeToSelect !== null ) {
       const children = nodeToSelect.children;
-      for (let i = 0; i < children.length; i++) {
-        if (children[i].data.name === nodePath[count].name &&
-          children[i].data.ringName === nodePath[count].ringName) {
-            nodeToSelect = children[i];
-            break;
-        } else {
-          if (i === children.length - 1) {
-            nodeToSelect = null;
+      if (children !== undefined) {
+        for (let i = 0; i < children.length; i++) {
+          if (children[i].data.name === nodePath[count].name &&
+            children[i].data.ringName === nodePath[count].ringName) {
+              nodeToSelect = children[i];
+              break;
+          } else {
+            if (i === children.length - 1) {
+              nodeToSelect = null;
+            }
           }
         }
+      } else {
+        nodeToSelect = null;
       }
       count--;
     }
