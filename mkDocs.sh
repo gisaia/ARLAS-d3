d592c9f8683f08938270ed426286705e23b402f9
mohamedhamougisaia <mohamed.hamou@gisaia.com>
2022-12-22 11:39:51 +0100

    Upgrade typedocgen version and add changelog

--- mkDocs.sh
+++ mkDocs.sh
@@ -6,13 +6,14 @@ mkdir target
 mkdir target/generated-docs
 
 ## GENERATE THE DOCUMENTATION ##
-docker run -a STDERR --rm -i -v `pwd`:/docs gisaia/typedocgen:0.0.5 generatedoc src
+docker run -a STDERR --rm -i -v `pwd`:/docs gisaia/typedocgen:0.0.7 generatedoc src
 
 ## MOVE ALL THE DOCUMENTATION TO THE 'generated-docs' FOLDER ##
 if [ -z "$(ls -A ./typedoc_docs)" ]; then
    echo "Documentation has not been generated"
 else
    mv typedoc_docs/* target/generated-docs
+   cp CHANGELOG.md target/generated-docs/CHANGELOG_ARLAS-d3.md
 fi
 if [ -d ./docs ] ; then
     cp -r docs/* target/generated-docs
