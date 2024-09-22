0961258095864d5616b205bd43de6c4d1718b810
Mohamed Hamou <mohamed.hamou@gisaia.com>
2024-09-22 17:50:33 +0200

    Revert "fix: no update of the brush when the brush is in use"
    This reverts commit d5e460232c8daa9f591499a3e791475879119e90.

--- src/histograms/charts/AbstractChart.ts
+++ src/histograms/charts/AbstractChart.ts
@@ -171,10 +171,6 @@ export abstract class AbstractChart extends AbstractHistogram {
   }
 
   public setSelectedInterval(selectedInputValues: SelectedInputValues): void {
-    if (this.brush?.isBrushed || this.brush?.isBrushing) {
-      return;
-    }
-
     this.checkSelectedValuesValidity(selectedInputValues);
     this.fromSetInterval = true;
     const parsedSelectedValues = HistogramUtils.parseSelectedValues(selectedInputValues, this.histogramParams.dataType);
@@ -251,9 +247,6 @@ export abstract class AbstractChart extends AbstractHistogram {
   }
 
   public redrawSelectedIntervals() {
-    if (this.brush?.isBrushed || this.brush?.isBrushing) {
-      return;
-    }
     const axes = this.getAxes();
     this.selectedBars.clear();
     this.histogramParams.selectionListIntervalId = [];
